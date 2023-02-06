c50adde13e7456c4f43b00370c80536d254b6c87
Mohamed Mansour <mohmans@pm.me>
2023-02-06 20:02:16 +0300

    add a store for timer

--- stores/timer.js
+++ stores/timer.js
@@ -1,13 +1,21 @@
 export const useTimerStore = defineStore('timer', {
   state: () => ({
     started: false,
+    timeRemaining: 120 // base time in seconds
   }),
   getters: {
     isStarted: (state) => state.started,
+    getTimeRemaining: (state) => state.timeRemaining
   },
   actions: {
     toggleTimer() {
       this.started = !this.started
     },
+    clearTimeRemaining() {
+      this.timeRemaining = 120
+    },
+    setTimeRemaining(time) {
+      this.timeRemaining = time
+    }
   },
 })
