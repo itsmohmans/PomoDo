bb83913993307b189e82956b973ee1a3c0650e1f
Mohamed Mansour <mohmans@pm.me>
2023-02-08 02:48:23 +0300

    add fullscreen toggle

--- components/toolbar.vue
+++ components/toolbar.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-toolbar density="compact" color="background">
+  <v-toolbar :collapse="isFullscreen" density="compact" color="background">
     <v-btn
+      v-if="!isFullscreen"
       v-for="(page, index) in pages"
       :key="index"
       :to="page.route"
@@ -13,18 +14,25 @@
       </v-tooltip>
     </v-btn>
 
-    <v-spacer></v-spacer>
+    <v-spacer v-if="!isFullscreen"></v-spacer>
 
-    <v-btn icon @click="useAppStore().toggleTheme()">
+    <v-btn v-if="!isFullscreen" icon @click="useAppStore().toggleTheme()">
       <v-icon>{{ useAppStore().getTheme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
       <v-tooltip activator="parent" location="bottom">
         Toggle Theme
+      </v-tooltip>
+    </v-btn>
+    <v-btn icon @click="toggle">
+      <v-icon>{{ isFullscreen ? 'mdi-arrow-collapse' : 'mdi-arrow-expand' }}</v-icon>
+      <v-tooltip activator="parent" location="bottom">
+        Toggle full screen
       </v-tooltip>
     </v-btn>
   </v-toolbar>
 </template>
 <script setup>
 import {useAppStore} from '/stores/app'
+const { isFullscreen, enter, exit, toggle } = useFullscreen()
 
 const pages = ref([
   {
