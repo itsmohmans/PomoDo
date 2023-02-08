76e5ad54e5a76b47ee942acf4660503620b74547
Mohamed Mansour <mohmans@pm.me>
2023-02-08 20:07:03 +0300

    remove settings page and improved toolbar

--- components/toolbar.vue
+++ components/toolbar.vue
@@ -1,28 +1,14 @@
 <template>
   <v-toolbar :collapse="isFullscreen" density="compact" color="background">
-    <v-btn
-      v-if="!isFullscreen"
-      v-for="(page, index) in pages"
-      :key="index"
-      :to="page.route"
-      :value="page.title"
-      icon
-    >
-      <v-icon>mdi-{{page.icon}}</v-icon>
-      <v-tooltip activator="parent" location="bottom" open-delay="500">
-        {{ page.title }}
-      </v-tooltip>
-    </v-btn>
-
+    <Settings v-if="!isFullscreen" />
     <v-spacer v-if="!isFullscreen"></v-spacer>
-
     <v-btn v-if="!isFullscreen" icon @click="useAppStore().toggleTheme()">
       <v-icon>{{ useAppStore().getTheme === 'light' ? 'mdi-weather-night' : 'mdi-weather-sunny' }}</v-icon>
       <v-tooltip activator="parent" location="bottom" open-delay="500">
         Toggle Theme
       </v-tooltip>
     </v-btn>
-    <v-btn icon @click="toggle">
+    <v-btn icon @click="toggleFullScreen">
       <v-icon>{{ isFullscreen ? 'mdi-arrow-collapse' : 'mdi-arrow-expand' }}</v-icon>
       <v-tooltip activator="parent" location="bottom" open-delay="500">
         Toggle full screen
@@ -32,23 +18,5 @@
 </template>
 <script setup>
 import {useAppStore} from '/stores/app'
-const { isFullscreen, enter, exit, toggle } = useFullscreen()
-
-const pages = ref([
-  {
-    title: 'Settings',
-    route: '/settings',
-    icon: 'cog'
-  },
-  {
-    title: 'Stats',
-    route: '/stats',
-    icon: 'chart-areaspline'
-  },
-  {
-    title: 'Timer',
-    route: '/',
-    icon: 'timer'
-  },
-])
+const { isFullscreen, toggle:toggleFullScreen } = useFullscreen()
 </script>
