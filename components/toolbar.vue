f640f22437ce88e5a764e2f7fe1e09893a5aa87e
Mohamed Mansour <mohmans@pm.me>
2023-03-04 01:10:12 +0300

    add toolbar button to open stats

--- components/toolbar.vue
+++ components/toolbar.vue
@@ -1,6 +1,21 @@
 <template>
   <v-toolbar :collapse="isFullscreen" density="compact" color="background">
     <Settings v-if="!isFullscreen" />
+    <v-btn
+      icon
+      :to="useRoute().name === 'stats' ? '/' : '/stats'"
+    >
+      <v-icon>
+        {{
+           useRoute().name === 'stats' ? 'mdi-timer' : 'mdi-chart-areaspline'
+        }}
+      </v-icon>
+      <v-tooltip activator="parent" location="bottom" open-delay="500">
+        {{
+           useRoute().name === 'stats' ? 'Timer' : 'Statistics'
+        }}
+      </v-tooltip>
+    </v-btn>
     <v-spacer v-if="!isFullscreen"></v-spacer>
     <v-btn v-if="!isFullscreen && useAppStore().showThemeToggle" icon @click="useAppStore().toggleTheme()">
       <v-icon>{{ useAppStore().getTheme === 'light' ? 'mdi-weather-night' : 'mdi-weather-sunny' }}</v-icon>
