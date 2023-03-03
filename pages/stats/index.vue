0c171e5ceaf1dc21acfe745eadd9da6ba20823bf
Mohamed Mansour <mohmans@pm.me>
2023-03-04 00:59:08 +0300

    first expirements with charts

--- pages/stats/index.vue
+++ pages/stats/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
-    stats page
+  <div class="stats-container">
+    <doughnut-chart :data="data" :options="options"/>
+    <line-chart class="line-chart" :data="data" :options="options"/>
+    <pie-chart :data="data" :options="options"/>
   </div>
 </template>
+<script setup>
+
+const data = {
+  labels: ['Cats', 'Dogs', 'Hamsters'],
+  datasets: [
+    {
+      label: 'First Dataset',
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF'],
+      data: [22, 11, 6]
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: true
+}
+
+
+</script>
+<style scoped>
+.stats-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  column-gap: 0;
+}
+.stats-container * {
+  max-width: 200px;
+  max-height: 200px;
+  margin: 0
+}
+.stats-container .line-chart {
+  max-width: 400px;
+}
+</style>
