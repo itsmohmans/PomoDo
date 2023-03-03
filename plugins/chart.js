4805bd053aa3ddd463904d8b9d313efd0941cb77
Mohamed Mansour <mohmans@pm.me>
2023-03-04 00:30:13 +0300

    import required line chart properties in chart plugin

--- plugins/chart.js
+++ plugins/chart.js
@@ -1,9 +1,27 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+} from 'chart.js';
 import { Doughnut, Line, Pie } from 'vue-chartjs';
 
 
 export default defineNuxtPlugin((nuxt) => {
-  ChartJS.register(ArcElement, Tooltip, Legend);
+  ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement
+  );
+  // register needed charts components
   nuxt.vueApp.component('doughnut-chart', Doughnut)
   nuxt.vueApp.component('line-chart', Line)
   nuxt.vueApp.component('pie-chart', Pie)
