bd25fbce88c17d03b6b5912d694811c5033e50f8
Mohamed Mansour <mohmans@pm.me>
2023-03-11 01:21:45 +0300

    configure and add data for minutes-label charts

--- pages/stats/index.vue
+++ pages/stats/index.vue
@@ -1,29 +1,101 @@
 <template>
   <div class="stats-container">
-    <doughnut-chart :data="data" :options="options"/>
-    <line-chart class="line-chart" :data="data" :options="options"/>
-    <pie-chart :data="data" :options="options"/>
+    <!-- <line-chart class="line-chart" :data="data" :options="options"/> -->
+    <pie-chart
+      class="chart mins-labels"
+      :data="minutesLabelCharts.data"
+      :options="minutesLabelCharts.options.pie"
+    />
+    <bar-chart
+      class="chart mins-labels"
+      :data="minutesLabelCharts.data"
+      :options="minutesLabelCharts.options.bar"
+    />
   </div>
 </template>
 <script setup>
+import { useStatsStore as stats } from '~~/stores/stats'
+import {useAppStore as app} from '~~/stores/app'
 
-const data = {
-  labels: ['Cats', 'Dogs', 'Hamsters'],
-  datasets: [
-    {
-      label: 'First Dataset',
-      backgroundColor: ['#41B883', '#E46651', '#00D8FF'],
-      data: [22, 11, 6]
+const { $dayjs } = useNuxtApp()
+
+const labels = stats().getLabels
+const dates = stats().getDates
+const minutes = Object.values(stats().getMinutes)
+  .map(arr => arr.reduce((total, mins) => total + mins/60, 0));
+const colors = Object.values(stats().getColors)
+
+const minutesLabelCharts = reactive({
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Hours',
+        backgroundColor: colors,
+        data: minutes,
+      },
+    ]
+  },
+  options: {
+    pie: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            color: app().getTheme === 'dark' ? '#fff' : '#000'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Minutes per label',
+        }
+      }
+    },
+    bar: {
+      responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Minutes per label',
+      }
     }
-  ]
-}
-
-const options = {
-  responsive: true,
-  maintainAspectRatio: true
-}
-
-
+    }
+  }
+})
+const minutesDaysCharts = reactive({
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Hours',
+        backgroundColor: colors,
+        data: minutes,
+      },
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: app().getTheme === 'dark' ? '#fff' : '#000'
+        }
+      },
+      title: {
+        display: true,
+        text: 'Minutes per label',
+      }
+    }
+  }
+})
 </script>
 <style scoped>
 .stats-container {
@@ -33,14 +105,19 @@ const options = {
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
-  column-gap: 0;
+  column-gap: 2rem;
 }
 .stats-container * {
-  max-width: 200px;
+  max-width: 400px;
   max-height: 200px;
   margin: 0
 }
 .stats-container .line-chart {
   max-width: 400px;
+}
+.chart.mins-labels {
+  padding: 10px;
+  border: 1px solid grey;
+  border-radius: 50px;
 }
 </style>
--- plugins/chart.js
+++ plugins/chart.js
@@ -3,25 +3,29 @@ import {
   ArcElement,
   Tooltip,
   Legend,
+  Title,
   CategoryScale,
   LinearScale,
   PointElement,
-  LineElement
+  LineElement,
+  BarElement,
 } from 'chart.js';
-import { Line, Pie } from 'vue-chartjs';
-
+import { Line, Pie, Bar } from 'vue-chartjs';
 
 export default defineNuxtPlugin((nuxt) => {
   ChartJS.register(
     ArcElement,
     Tooltip,
     Legend,
+    Title,
     CategoryScale,
     LinearScale,
     PointElement,
-    LineElement
+    LineElement,
+    BarElement,
   );
   // register needed charts components
   nuxt.vueApp.component('line-chart', Line)
   nuxt.vueApp.component('pie-chart', Pie)
+  nuxt.vueApp.component('bar-chart', Bar)
 })
