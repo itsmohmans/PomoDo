813d9e1b07a167908ad62cd799092795dfd43ac9
Mohamed Mansour <mohmans@pm.me>
2023-06-14 18:13:57 +0300

    fix: clear time remaining on startup

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,10 @@
   </div>
 </template>
 <script setup>
+import { useTimerStore as timer } from '~~/stores/timer';
+
 onMounted(() => {
   getSettings()
+  timer().clearTimeRemaining()
 })
 </script>
